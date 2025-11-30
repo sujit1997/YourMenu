4371096d2695fc76a4f246714049fbaf9d5f0221
sujit <sujitmanmode1997@gmail.com>
2025-11-30 15:04:18 +0530

    New Your Menu

---
+++ src/main/java/resources/TS_01_TC_001_BrowserLoading.java
@@ -0,0 +1,32 @@
+package resources;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class TS_01_TC_001_BrowserLoading {
+    public static String browser = "chrome";
+    public static WebDriver driver;
+
+    public static void main(String[] args) {
+        if (browser.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+        else if (browser.equalsIgnoreCase("firefox")){
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        }
+        driver.manage().window().maximize();
+        driver.get("https://hotel-example-site.takeyaqa.dev/en-US/index.html");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.quit();
+    }
+}
